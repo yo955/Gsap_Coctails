b1250da31d40cb51c486689bedf7dd2f09a87892
yo955 <youssefshahban2001@gmail.com>
2025-08-26 07:02:33 -0700

    add public file and Navbar Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from "./components/Navbar";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className=" flex-center min-h-screen">App</div>
+    <main >
+      <Navbar />
+    </main>
   )
 }
 
